dbdb47d73f8c962e5a189879921af88efc21600e
kinngh <kinngh@me.com>
2022-06-16 11:55:10 +0530

    ⭐ App Proxy Setup II

--- server/middleware/proxyVerification.js
+++ server/middleware/proxyVerification.js
@@ -1,19 +1,19 @@
 const crypto = require("crypto");
-const querystring = require("querystring");
 
 const proxyVerification = (req, res, next) => {
   const { signature } = req.query;
 
-  const queryString = req._parsedUrl.query
+  const queryURI = req._parsedUrl.query
     .replace("/?", "")
     .replace(/&signature=[^&]*/, "")
     .split("&")
-    .map((x) => querystring.unescape(x))
+    .map((x) => decodeURIComponent(x))
     .sort()
     .join("");
+
   const calculatedSignature = crypto
     .createHmac("sha256", process.env.SHOPIFY_API_SECRET)
-    .update(queryString, "utf-8")
+    .update(queryURI, "utf-8")
     .digest("hex");
 
   if (calculatedSignature === signature) {
