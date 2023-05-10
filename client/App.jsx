1bd8e73857087db84e5cee3742158aa4dbdadef9
Kinngh <kinngh@me.com>
2023-05-10 12:02:21 +0530

    🔨 Fix navigation issues, speed up navigation link

--- client/App.jsx
+++ client/App.jsx
@@ -1,13 +1,11 @@
-import {
-  NavigationMenu,
-  Provider as AppBridgeProvider,
-} from "@shopify/app-bridge-react";
+import { NavigationMenu } from "@shopify/app-bridge-react";
 import { AppProvider as PolarisProvider } from "@shopify/polaris";
 import "@shopify/polaris/build/esm/styles.css";
 import translations from "@shopify/polaris/locales/en.json";
-import { useRoutes } from "raviger";
-import ApolloClientProvider from "./providers/ApolloClientProvider";
+import { usePath, useRoutes } from "raviger";
 import routes from "./Routes";
+import ApolloClientProvider from "./providers/ApolloClientProvider";
+import AppBridgeProvider from "./providers/AppBridgeProvider";
 
 const appBridgeConfig = {
   apiKey: process.env.SHOPIFY_API_KEY,
@@ -16,18 +14,24 @@ const appBridgeConfig = {
 };
 
 export default function App() {
+  const currentPath = usePath();
   const RouteComponents = useRoutes(routes);
 
   return (
     <PolarisProvider i18n={translations}>
-      <AppBridgeProvider config={appBridgeConfig}>
+      <AppBridgeProvider>
         <NavigationMenu
           navigationLinks={[
             {
-              label: "Debug Cards",
-              destination: "/debug",
+              label: "Fetch Data",
+              destination: "/debug/getData",
+            },
+            {
+              label: "Billing API",
+              destination: "/debug/billing",
             },
           ]}
+          matcher={(link) => currentPath === link.destination}
         />
         <ApolloClientProvider>{RouteComponents}</ApolloClientProvider>
       </AppBridgeProvider>
---
+++ client/providers/AppBridgeProvider.jsx
@@ -0,0 +1,58 @@
+import { Provider } from "@shopify/app-bridge-react";
+import { Layout, Page, Spinner } from "@shopify/polaris";
+import { useNavigate, usePath, useQueryParams } from "raviger";
+import { useEffect, useMemo, useState } from "react";
+
+function AppBridgeProvider({ children }) {
+  const navigate = useNavigate();
+  const location = usePath();
+  const [query] = useQueryParams();
+
+  const [appBridgeConfig, setConfig] = useState(null);
+
+  useEffect(() => {
+    const host = query?.host;
+
+    if (host) {
+      setConfig({
+        host: host,
+        apiKey: process.env.SHOPIFY_API_KEY,
+        forceRedirect: true,
+      });
+    }
+  }, [query]);
+
+  const history = useMemo(
+    () => ({
+      replace: (path) => {
+        navigate(path);
+      },
+    }),
+    [location]
+  );
+
+  const routerConfig = useMemo(
+    () => ({ history, location }),
+    [history, location]
+  );
+
+  if (!appBridgeConfig) {
+    return (
+      <Page>
+        <Layout>
+          <Layout.Section>
+            <Spinner />
+          </Layout.Section>
+        </Layout>
+      </Page>
+    );
+  }
+
+  return (
+    <Provider config={appBridgeConfig} router={routerConfig}>
+      {children}
+    </Provider>
+  );
+}
+
+export default AppBridgeProvider;
